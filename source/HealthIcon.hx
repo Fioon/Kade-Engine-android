be0e3a5c65fc41d0da84ae294fb68e8c8e16f4f3
Fioon <135500399+Fioon@users.noreply.github.com>
2024-03-03 14:02:39 +0800

    Update HealthIcon.hx

--- source/HealthIcon.hx
+++ source/HealthIcon.hx
@@ -37,6 +37,8 @@ class HealthIcon extends FlxSprite
 		animation.add('parents-christmas', [17, 18], 0, false, isPlayer);
 		animation.add('monster', [19, 20], 0, false, isPlayer);
 		animation.add('monster-christmas', [19, 20], 0, false, isPlayer);
+		animation.add('ep-bf', [10, 11], 0, false, isPlayer);
+		animation.add('ep-mouse', [10, 11], 0, false, isPlayer);
 		animation.play(char);
 
 		switch(char)
